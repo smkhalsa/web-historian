73e72c74f2f7b61c3033f53e1edd12ca7d763f56
smkhalsa <smkhalsa@gmail.com>
2015-06-03 16:25:09 -0700

    Passed first test

--- web/basic-server.js
+++ web/basic-server.js
@@ -1,13 +1,28 @@
 var http = require("http");
 var handler = require("./request-handler");
 var initialize = require("./initialize.js");
-
+var url = require('url');
 // Why do you think we have this here?
 // HINT:It has to do with what's in .gitignore
 initialize();
 
+// var routes = {
+//   '/': handler.handleRequest,
+//   '/styles.css': handler.handleRequest
+// };
+
 var port = 8080;
 var ip = "127.0.0.1";
-var server = http.createServer(handler.handleRequest);
+var server = http.createServer(function(req, res) {
+  var path = url.parse(req.url).pathname;
+
+  if (path.substr(0, 3) === 'www') {
+    handler.serveSites(req, res, path);
+  } else {
+    handler.handleRequest(req, res, path);
+  }
+});
+
+
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,51 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var staticServe = require('node-static');
+var fs = require('fs');
 // require more modules/folders here!
+// archive.siteAssets
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+exports.handleRequest = function (req, res, path) {
+  // res.end(archive.paths.list);
+  var action = req.method;
+  if (path === '/' || path === undefined) {
+    path = '/index.html';
+  }
+
+  var serveGet = function() {
+    var statusCode = 200;
+    var filePath = archive.paths.siteAssets+ path;
+    fs.readFile(filePath, {encoding: 'utf8'}, function(error, data){
+      if (error) {
+        console.log('error: ' + error)
+      }
+
+      res.writeHead(statusCode, {'Content-Type': 'text/html'});
+      console.log(data);
+      res.end(data);
+    });
+  };
+    // var server = new staticServe.Server(archive.paths['siteAssets']);
+
+    // req.addListener('end', function () {
+    //     server.serve(req, res);
+    //   }).resume();
+
+  var actions = {
+    'GET': serveGet
+  };
+
+  if(actions[action]) {
+    actions[action]();
+  }
+
+
+};
+
+
+exports.serveSites = function(req, res) {
+  //add to paths list if not there
+    //takes user to loading page OR responds with archived site
+
 };
